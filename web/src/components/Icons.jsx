05903c6194d574f5aa62dfc4689f785e28481aef
VikusS3 <felix21soc@gmail.com>
2024-07-10 10:44:14 -0500

    remove icon

--- web/src/components/Icons.jsx
+++ web/src/components/Icons.jsx
@@ -144,31 +144,10 @@ export const Netlify = (props) => (
     <path
       fill="#05BDBA"
       d="M71.677 122.889H1.466L0 121.423V103.83l1.466-1.466h70.211l1.466 1.466v17.593zM254.534 122.889h-70.211l-1.466-1.466V103.83l1.466-1.466h70.211L256 103.83v17.593zM117.876 54.124V1.466L119.342 0h17.593l1.466 1.466v52.658l-1.466 1.466h-17.593zM117.876 223.787v-52.658l1.466-1.466h17.593l1.466 1.466v52.658l-1.466 1.465h-17.593z"
-    />
+    />d
   </svg>
 );
 
-export const FingerDown = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="icon icon-tabler icons-tabler-outline icon-tabler-hand-finger"
-    {...props}
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M8 13v-8.5a1.5 1.5 0 0 1 3 0v7.5" />
-    <path d="M11 11.5v-2a1.5 1.5 0 1 1 3 0v2.5" />
-    <path d="M14 10.5a1.5 1.5 0 0 1 3 0v1.5" />
-    <path d="M17 11.5a1.5 1.5 0 0 1 3 0v4.5a6 6 0 0 1 -6 6h-2h.208a6 6 0 0 1 -5.012 -2.7a69.74 69.74 0 0 1 -.196 -.3c-.312 -.479 -1.407 -2.388 -3.286 -5.728a1.5 1.5 0 0 1 .536 -2.022a1.867 1.867 0 0 1 2.28 .28l1.47 1.47" />
-  </svg>
-);
 
 export const Github = (props) => (
   <svg
